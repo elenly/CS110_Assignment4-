46cce225d2e315ddb3994559aa199df176dd85bf
elenly <elen.24.ly@gmail.com>
2022-11-16 20:46:13 +0400

    plusfigure

--- plusfigure.js
+++ plusfigure.js
@@ -1,1 +1,20 @@
-g
+function plusfigure(s, p){
+    let string = "";
+    for(let i = 0; i < s; i++){
+        string += " ";
+    }
+    for(let j = 0; j < p; j++){
+        string += "+";
+    }
+    return string;
+}
+
+const num = 5;
+
+let space = num - 1;
+
+while(space >= 0){
+    let plus = num - space;
+    console.log(plusfigure(space, plus));
+    space--;
+}
