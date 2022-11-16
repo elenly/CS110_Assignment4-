3c48e3f4b2622c8fd1153c252516e0e139f0a4a9
elenly <elen.24.ly@gmail.com>
2022-11-16 20:59:53 +0400

    factorial

---
+++ factorial.js
@@ -0,0 +1,16 @@
+const num =5;
+
+function factorial(num){
+    
+    let factorial = 1;
+
+    for(let count = 2; count <= num; count++){
+        factorial *= count; 
+    }
+    return factorial;
+}
+
+console.log(factorial(num))
+//module.exports = {
+  //  factorial: factorial
+//}
